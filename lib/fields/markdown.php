b91b9c5c702aaa574a384cf274d6a93d547410eb
Fabian Michael <hallo@fabianmichael.de>
2018-12-14 12:18:26 +0100

    Replace test option with font option.

--- lib/fields/markdown.php
+++ lib/fields/markdown.php
@@ -6,13 +6,16 @@ $options = require kirby()->root('kirby') . '/config/fields/textarea.php';
 /* Merge new properties
 --------------------------------*/
 
-$options = array_merge_recursive($options, array(
-    'props' => array(
-        'test' => function($test = 'This is a markdown editor') {
-            return $test;
+$options = array_merge_recursive($options, [
+    'props' => [
+        /**
+         * Sets the editor font. Allowed values: monospace, sans-serif
+         */
+        'font' => function($font = 'monospace') {
+            return $font;
         },
-    ),
-));
+    ],
+]);
 
 // return the updated options
 return $options;
