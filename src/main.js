5b40489dc3c5c9c2ee9190b32c5cabd014bf55bf
Sylvain Julé <sylvain.jule@free.fr>
2018-12-27 15:24:29 +0100

    Sort component registration alphabetically

--- src/main.js
+++ src/main.js
@@ -9,24 +9,24 @@ import './assets/icons/icons.js'
 
 import Markdown       from './components/field/Markdown.vue'
 import MarkdownInput  from './components/input/MarkdownInput.vue'
-
-import Headlines      from './components/toolbar/buttons/headlines.vue'
 import Button         from './components/toolbar/buttons/button.vue'
-import Bold           from './components/toolbar/buttons/bold.vue'
-import Italic         from './components/toolbar/buttons/italic.vue'
-import Code           from './components/toolbar/buttons/code.vue'
-import Link           from './components/toolbar/buttons/link.vue'
-import PageLink       from './components/toolbar/buttons/pagelink.vue'
-import Email          from './components/toolbar/buttons/email.vue'
-import Ul             from './components/toolbar/buttons/ul.vue'
-import Ol             from './components/toolbar/buttons/ol.vue'
-import Divider        from './components/toolbar/buttons/divider.vue'
+
 import Blockquote     from './components/toolbar/buttons/blockquote.vue'
-import HorizontalRule from './components/toolbar/buttons/horizontal-rule.vue'
-import Invisibles     from './components/toolbar/buttons/invisibles.vue'
-import Image          from './components/toolbar/buttons/image.vue'
+import Bold           from './components/toolbar/buttons/bold.vue'
+import Code           from './components/toolbar/buttons/code.vue'
+import Divider        from './components/toolbar/buttons/divider.vue'
+import Email          from './components/toolbar/buttons/email.vue'
 import File           from './components/toolbar/buttons/file.vue'
+import Headlines      from './components/toolbar/buttons/headlines.vue'
+import HorizontalRule from './components/toolbar/buttons/horizontal-rule.vue'
+import Image          from './components/toolbar/buttons/image.vue'
+import Invisibles     from './components/toolbar/buttons/invisibles.vue'
+import Italic         from './components/toolbar/buttons/italic.vue'
+import Link           from './components/toolbar/buttons/link.vue'
+import Ol             from './components/toolbar/buttons/ol.vue'
+import PageLink       from './components/toolbar/buttons/pagelink.vue'
 import Strikethrough  from './components/toolbar/buttons/strikethrough.vue'
+import Ul             from './components/toolbar/buttons/ul.vue'
 
 
 panel.plugin('community/markdown-field', {
@@ -34,23 +34,25 @@ panel.plugin('community/markdown-field', {
         markdown: Markdown,
     },
     components: {
+    	// Main components
         'k-markdown-input': MarkdownInput,
         'k-markdown-button': Button,
-        'k-markdown-button-headlines': Headlines,
-        'k-markdown-button-bold': Bold,
-        'k-markdown-button-italic': Italic,
-        'k-markdown-button-code': Code,
-        'k-markdown-button-link': Link,
-        'k-markdown-button-pagelink': PageLink,
-        'k-markdown-button-email': Email,
-        'k-markdown-button-ul': Ul,
-        'k-markdown-button-ol': Ol,
-        'k-markdown-button-divider': Divider,
+        // Custom buttons
         'k-markdown-button-blockquote': Blockquote,
-        'k-markdown-button-horizontal-rule': HorizontalRule,
-        'k-markdown-button-invisibles': Invisibles,
-        'k-markdown-button-image': Image,
+        'k-markdown-button-bold': Bold,
+        'k-markdown-button-code': Code,
+        'k-markdown-button-divider': Divider,
+        'k-markdown-button-email': Email,
         'k-markdown-button-file': File,
+        'k-markdown-button-headlines': Headlines,
+        'k-markdown-button-horizontal-rule': HorizontalRule,
+        'k-markdown-button-image': Image,
+        'k-markdown-button-invisibles': Invisibles,
+        'k-markdown-button-italic': Italic,
+        'k-markdown-button-link': Link,
+        'k-markdown-button-ol': Ol,
+        'k-markdown-button-pagelink': PageLink,
         'k-markdown-button-strikethrough': Strikethrough,
+        'k-markdown-button-ul': Ul,
     }
 });
