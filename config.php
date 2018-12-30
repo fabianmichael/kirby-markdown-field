1eec0f10eade239abc1d4c7163299ecb2f0517bf
Sylvain Julé <sylvain.jule@free.fr>
2018-12-31 00:45:01 +0100

    Get rid of field methods inserted by error

--- config.php
+++ config.php
@@ -4,15 +4,6 @@ Kirby::plugin('community/markdown-field', [
     'fields' => array(
         'markdown' => require_once __DIR__ . '/lib/fields/markdown.php',
     ),
-    'fieldMethods' => array(
-    	'toLabel' => function($field) {
-    		$value = $field->value;
-    		return '<span class="category-label" data-category="'. $value .'">' . $value . '</span>';
-    	},
-    	'toBlue' => function($field) {
-            return '<span style="color: #384d9d">' . $field->value . '</span>';
-        }
-    ),
     'translations' => array(
         'en' => require_once __DIR__ . '/lib/languages/en.php',
         'fr' => require_once __DIR__ . '/lib/languages/fr.php',
