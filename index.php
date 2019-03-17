eec14c947f20d6930e1448d80832ba5aca1696fc
Sylvain Julé <sylvain.jule@free.fr>
2019-03-17 17:48:15 +0100

    Follow the new Pluginkit guidelines

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+Kirby::plugin('community/markdown-field', [
+	'options' => array(
+		'buttons'    => ['headlines', 'bold', 'italic', 'divider', 'link', 'email', 'code', 'divider', 'ul', 'ol'],
+		'font'       => [
+			'family'  => 'monospace',
+			'size'    => 'regular',
+			'scaling' => false,
+		],
+		'query'      => [
+			'pagelink' => null,
+			'images'   => 'page.images',
+			'files'    => 'page.files.filterBy("type", "!=", "image")',
+		],
+		'modals'     => true,
+		'blank'      => false,
+		'invisibles' => false,
+	),
+    'fields' => array(
+        'markdown' => require_once __DIR__ . '/lib/fields/markdown.php',
+    ),
+    'translations' => array(
+        'en' => require_once __DIR__ . '/lib/languages/en.php',
+        'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+        'de' => require_once __DIR__ . '/lib/languages/de.php',
+    ),
+]);
