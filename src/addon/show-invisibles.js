f69d19cb37083cebee4772b3d4cc65b7bcb49510
Fabian Michael <hallo@fabianmichael.de>
2019-05-10 10:53:18 +0200

    Better handling of ZERO WIDTH SPACE (U+200B) character.

--- src/addon/show-invisibles.js
+++ src/addon/show-invisibles.js
@@ -7,15 +7,15 @@ import CodeMirror from 'codemirror';
 // Otherwise, the editor would highlight a soft-hyphen as an
 // invalid character, which is quote okay for source code,
 // but not for actual editorial content like Markdown.
-const DEFAULT_SPECIAL_CHARS = /[\u0000-\u001f\u007f-\u009f\u061c\u200b-\u200f\u2028\u2029\ufeff]/;
+const DEFAULT_SPECIAL_CHARS = /[\u0000-\u001f\u007f-\u009f\u061c\u200c-\u200f\u2028\u2029\ufeff]/;
 
 // Override defaults to prevent the Soft Hyphen from getting displayed as a red dot (invalid char).
 CodeMirror.defaults.specialChars = DEFAULT_SPECIAL_CHARS;
 
 CodeMirror.defineOption('showInvisibles', false, (cm, val, old) => {
-  
+
   const prev = old == CodeMirror.Init ? false : old;
-  
+
   if (val == prev) {
     return;
   }
@@ -32,15 +32,15 @@ CodeMirror.defineOption('showInvisibles', false, (cm, val, old) => {
       '\u2029', // Parapgraph Separator
       '\ufeff', // Byte Order Mark
     ];
-    
+
     cm.getWrapperElement().classList.add('cm-show-invisibles');
     cm.setOption('specialChars', new RegExp(`[${ranges.join('')}]`));
     cm.setOption('specialCharPlaceholder', (char) => {
-      
+
       const node = document.createElement('span');
-      
+
       let cls = 'cm-ch';
-      
+
       if(char == '\u00A0') {
         // no-break space
         cls += ' cm-ch-nbsp';
@@ -50,16 +50,19 @@ CodeMirror.defineOption('showInvisibles', false, (cm, val, old) => {
         cls += ' cm-ch-space';
         node.textContent = char;
       } else if (char == '\u00ad') {
-        // no-break space
+        // soft hyphen
         node.textContent = char;
         cls += ' cm-ch-shy';
+      } else if (char == '\u200b') {
+        node.textContent = char;
+        cls += ' cm-ch-zero-width-space';
       } else {
         // Other special chars
         node.textContent = '•';
       }
-  
+
       node.className = cls;
-  
+
       return node;
     });
   } else {
