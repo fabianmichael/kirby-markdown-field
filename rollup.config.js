ada2a4d2cc93f7a97b6f46b58e08c0c65efd67ab
Fabian Michael <hallo@fabianmichael.de>
2021-03-04 12:19:59 +0100

    Add rollup to fix production builds

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+import commonjs from 'rollup-plugin-commonjs'
+import vue from 'rollup-plugin-vue'
+import { terser } from "rollup-plugin-terser"
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano';
+
+export default {
+  input: "./src/index.js",
+  output: {
+    file: "./index.js",
+    format: "iife"
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    postcss({
+      extract: true,
+      plugins: [
+        autoprefixer(),
+        cssnano(),
+      ],
+    }),
+    vue({ css: false }),
+    terser(),
+  ],
+}
