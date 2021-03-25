c7eed3c42d4e7767d435f33cfef2dbc7fd2b8b13
Fabian Michael <hallo@fabianmichael.de>
2021-03-25 11:55:11 +0100

    Add example for custom highlights

---
+++ extensions-examples/custom-highlights/index.php
@@ -0,0 +1,35 @@
+<?php
+
+Kirby::plugin('my/custom-highlights', [
+  'community.markdown-field.customHighlights' => [
+    /**
+     * Simple highlight, using regex.
+     */
+    [
+      'name'       => 'mark', // there can be multiple highlights with the same name
+      'regex'      => '<mark>.*</mark>',
+      'flags'      => 'i', // 'g' flag is added automatically
+      'class'      => 'my-mark-highlight',
+    ],
+    /**
+     * Advanced highliht
+     */
+    function() {
+
+        // Array of known text variables, should be fetched from some
+        // data source in a real pluign. Allows for validation, because
+        // only known variables will be highlighted properly.
+        $knownVariables = [
+          'disclaimer',
+          'support-me',
+          'copyright-info'
+        ];
+
+        return [
+            'name'       => 'variables',
+            'regex'      => '\{% (' . implode('|', $knownVariables). ') \%}',
+            'class'      => 'my-text-variable',
+        ];
+    },
+  ]
+]);
