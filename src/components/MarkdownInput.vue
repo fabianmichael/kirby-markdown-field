958f124e9121f119b0b58598dbd76506e67da836
Thomas Günther <mail@medienbaecker.com>
2021-10-22 12:04:10 +0200

    Backwards compatibility for Kirby < 3.6

--- src/components/MarkdownInput.vue
+++ src/components/MarkdownInput.vue
@@ -126,6 +126,10 @@ export default {
   },
   computed: {
     currentLanguage() {
+      if(this.$store.state.languages) {
+        // backwards compatibility for Kirby < 3.6
+        return this.$store.state.languages.current;
+      }
       return this.$language;
     },
   },
