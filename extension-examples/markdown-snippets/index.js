cac0893cb8445897fcae1f5053fd879054f0766d
Fabian Michael <hallo@fabianmichael.de>
2022-03-24 11:40:31 +0100

    Add snippets plugin example.

---
+++ extension-examples/markdown-snippets/index.js
@@ -0,0 +1,62 @@
+
+// Dropdown with text snippets
+(function() {
+
+  // Ensure, that the global `markdownEditorButtons` variable exists, otherwise
+  // declare it. It is just a plain array, that gets read whenever the field is use.
+  window.markdownEditorButtons = window.markdownEditorButtons || [];
+
+  // Pass the plugin definition to the buttons array
+  window.markdownEditorButtons.push({
+
+    /**
+     * This button has a dropdown, which contents are derived from
+     * field config in your blueprint:
+     *
+     * text:
+     *   type: markdown
+     *   buttons:
+     *     - bold
+     *     - italic
+     *     snippets:
+     *      - value: "(alert: My text color: red)"
+     *        text: Alert box
+     *      - value: ":-)"
+     *        text: "Smiley"
+     *      - value: "Ⓐ"
+     *        text: "Anarchy symbol"
+     */
+    configure(options) {
+      if (Array.isArray(options)) {
+        // transform options array into commands that can be
+        // understood by the editor plugin
+        dropdown = options.map(({ text: label, value }) => ({
+          label,
+          command: () => this.editor.insert(value)
+        }))
+
+        this.options = {
+          ...this.defaults,
+          dropdown
+        }
+      }
+    },
+
+    get button() {
+      return {
+        icon: "bolt",
+        label: "Snippets",
+        dropdown
+      }
+    },
+
+    get isDisabled() {
+      return () => false
+    },
+
+    get name() {
+      return "snippets"
+    },
+  })
+
+})();
---
+++ extension-examples/markdown-snippets/index.php
@@ -0,0 +1,5 @@
+<?php
+
+use Kirby\Cms\App;
+
+App::plugin('my/markdown-snippets', []);
