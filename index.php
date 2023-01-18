4394eba431d8c091d525e7b683c0660b8a5811b6
Fabian Michael <hallo@fabianmichael.de>
2023-01-18 17:48:35 +0100

    Remove strict version check

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
 
-use Kirby\Cms\App as Kirby;
+use Kirby\Cms\App;
 
-// validate the Kirby version; the supported versions are
-// updated manually when verified to work with the plugin
-$kirbyVersion = Kirby::version();
-
-if (
-    $kirbyVersion !== null &&
-    (version_compare($kirbyVersion, '3.6.0-rc.2', '<') === true ||
-        version_compare($kirbyVersion, '3.9.0-alpha', '>=') === true)
-) {
-    throw new Exception(
-        'The installed version of the Markdown field plugin ' .
-            'is not compatible with Kirby ' . $kirbyVersion
-    );
-}
-
-Kirby::plugin('fabianmichael/markdown-field', [
+App::plugin('fabianmichael/markdown-field', [
     'blueprints' => [
         'blocks/markdown' => __DIR__ . '/blueprints/blocks/markdown.yml',
     ],
