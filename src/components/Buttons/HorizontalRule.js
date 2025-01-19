cd03b95b8fef8f6ab845bf1384cd8fba1d091d06
Fabian Michael <hallo@fabianmichael.de>
2025-01-19 22:53:48 +0100

    refactor: get ready for Kirby 5 compatibility
    - follow standard.js style, add prettier config
    - reformat alsl CSS and JS files
    - replace tabs with spaces
    - also tweak a few colors for dark mode

--- src/components/Buttons/HorizontalRule.js
+++ src/components/Buttons/HorizontalRule.js
@@ -1,85 +1,85 @@
-import { EditorSelection } from "@codemirror/state";
-import Button from "./Button.js";
-import { ltrim, rtrim } from "../Utils/strings.js";
+import { EditorSelection } from '@codemirror/state'
+import Button from './Button.js'
+import { ltrim, rtrim } from '../Utils/strings.js'
 
 export default class HorizontalRule extends Button {
-	get button() {
-		return {
-			icon: "separator",
-			label: this.input.$t("markdown.toolbar.button.hr"),
-			command: this.command
-		};
-	}
+  get button () {
+    return {
+      icon: 'separator',
+      label: this.input.$t('markdown.toolbar.button.hr'),
+      command: this.command
+    }
+  }
 
-	get command() {
-		return () => {
-			const { view } = this.editor;
-			const { state } = view;
-			const selection = state.selection.main;
-			let textBefore = rtrim(state.doc.slice(0, selection.from).toString());
-			let textAfter = ltrim(state.doc.slice(selection.to).toString());
+  get command () {
+    return () => {
+      const { view } = this.editor
+      const { state } = view
+      const selection = state.selection.main
+      let textBefore = rtrim(state.doc.slice(0, selection.from).toString())
+      let textAfter = ltrim(state.doc.slice(selection.to).toString())
 
-			textBefore =
-				textBefore +
-				(textBefore.length > 0 ? "\n\n" : "") +
-				this.syntax.render();
-			textAfter = "\n\n" + textAfter;
+      textBefore =
+        textBefore +
+        (textBefore.length > 0 ? '\n\n' : '') +
+        this.syntax.render()
+      textAfter = '\n\n' + textAfter
 
-			view.dispatch({
-				changes: {
-					from: 0,
-					to: state.doc.length,
-					insert: textBefore + textAfter
-				},
-				selection: EditorSelection.cursor(textBefore.length),
-				scrollIntoView: true
-			});
-		};
-	}
+      view.dispatch({
+        changes: {
+          from: 0,
+          to: state.doc.length,
+          insert: textBefore + textAfter
+        },
+        selection: EditorSelection.cursor(textBefore.length),
+        scrollIntoView: true
+      })
+    }
+  }
 
-	configure(options) {
-		if (typeof options === "string") {
-			options = { mark: options };
-		}
+  configure (options) {
+    if (typeof options === 'string') {
+      options = { mark: options }
+    }
 
-		Button.prototype.configure.call(this, options);
+    Button.prototype.configure.call(this, options)
 
-		if (!["***", "---", "___"].includes(this.options.mark)) {
-			throw "Horizontal rule mark must be either `***`, `---` or `___`.";
-		}
-	}
+    if (!['***', '---', '___'].includes(this.options.mark)) {
+      throw new Error('Horizontal rule mark must be either `***`, `---` or `___`.')
+    }
+  }
 
-	get defaults() {
-		return {
-			mark: "***"
-		};
-	}
+  get defaults () {
+    return {
+      mark: '***'
+    }
+  }
 
-	get name() {
-		return "hr";
-	}
+  get name () {
+    return 'hr'
+  }
 
-	get syntax() {
-		return {
-			token: this.token,
-			type: this.tokenType,
-			render: () => this.options.mark
-		};
-	}
+  get syntax () {
+    return {
+      token: this.token,
+      type: this.tokenType,
+      render: () => this.options.mark
+    }
+  }
 
-	get token() {
-		return "HorizontalRule";
-	}
+  get token () {
+    return 'HorizontalRule'
+  }
 
-	get tokenType() {
-		return "block";
-	}
+  get tokenType () {
+    return 'block'
+  }
 
-	get isActive() {
-		return () => false;
-	}
+  get isActive () {
+    return () => false
+  }
 
-	get isDisabled() {
-		return () => this.editor.isActiveToken("Kirbytag", "Link");
-	}
+  get isDisabled () {
+    return () => this.editor.isActiveToken('Kirbytag', 'Link')
+  }
 }
